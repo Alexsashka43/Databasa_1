090ef7d54613b1b402a5a5cdd5313bd022e94921
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2021-06-26 15:52:13 +0300

    hw2

--- phones/views.py
+++ phones/views.py
@@ -1,13 +1,57 @@
 from django.shortcuts import render
 
+from django.template.defaultfilters import slugify
+
+from .models import Phone
+from .management.commands.import_phones import Command
+
+
+command = Command()
+
+
+response = command.get_phones()
+
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+
+    if sort == 'max_price':
+        val = Phone.objects.order_by('-price')
+        context = {
+            'phones': val,
+            'sort': sort
+        }
+        return render(request, template, context)
+    elif sort == 'min_price':
+        val = Phone.objects.order_by('price')
+        context = {
+            'phones': val,
+            'sort': sort
+        }
+        return render(request, template, context)
+    elif sort == 'name':
+
+        val = Phone.objects.order_by('name')
+        context = {
+            'phones': val,
+            'sort': sort
+        }
+        return render(request, template, context)
+    val = Phone.objects.order_by('price')
+    context = {
+        'phones': val,
+        'sort': sort
+    }
     return render(request, template, context)
 
 
-def show_product(request, slug):
+def show_product(request, slug: str):
+    print(slug)
+
+
     template = 'product.html'
-    context = {}
+    context = {
+        'product': Phone.objects.filter(slug=slug).first()
+    }
     return render(request, template, context)
